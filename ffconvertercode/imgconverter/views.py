89f1627b2223743eb2dad7f64f6d67c58620d7eb
Hritik Tayade <braceritchie@protonmail.com>
2018-12-13 19:26:04 +0530

    updates

--- ffconvertercode/imgconverter/views.py
+++ ffconvertercode/imgconverter/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def conv(request,slug):
-    return HttpResponse(slug)
+    return render(request, "imgconverter/home.html")
