da3de23b59c35c55890edea7f3b89f8e33420e6b
Hritik Tayade <braceritchie@protonmail.com>
2018-12-14 18:32:22 +0530

    jpeg converter works

--- ffconvertercode/imgconverter/models.py
+++ ffconvertercode/imgconverter/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 
-class Image(models.Model):
+class Image1(models.Model):
     ImageFile = models.ImageField()
--- ffconvertercode/imgconverter/views.py
+++ ffconvertercode/imgconverter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .forms import FileField
-from .models import Image
+from .models import Image1
+from PIL import Image 
+from django.conf import settings
 # Create your views here.
 def conv(request,slug):
     if request.method == 'POST':
@@ -9,10 +11,26 @@ def conv(request,slug):
         
         if(form.is_valid()):
             
-            file = Image(ImageFile = request.FILES['file'])
+            file = Image1(ImageFile = request.FILES['file'])
             file.save()
+            download = convertFile(file)
             path = file.ImageFile.url
-            return render(request, "imgconverter/home.html",{'slug':slug,'form':form,'path':path})
+            
+            return render(request, "imgconverter/home.html",{'slug':slug,'form':form,'path':path,'download':download})
 
     form = FileField()
     return render(request, "imgconverter/home.html",{'slug':slug,'form':form})
+
+
+def convertFile(im):
+        newIm = im.ImageFile
+        path = im.ImageFile.url
+        name = newIm.name
+        name = name.split(".")
+        print(name)
+        Pim = Image.open(newIm)
+        rgb_im = Pim.convert('RGB')
+        rgb_im.save("media/"+name[0]+".jpg")
+        downloadpath = path.split(".")
+        return downloadpath[0]+".jpg"
+        
